356613a5c2b48cad26a147cea594f20147098261
Jonas Bandi <dev@jonasbandi.net>
2025-09-10 01:07:33 +0200

    Consolidation

--- 02-chatbot-react-solution/src/tools/tools.ts
+++ 02-chatbot-react-solution/src/tools/tools.ts
@@ -8,6 +8,7 @@ export function createTools() {
       inputSchema: z.object({}),
       execute: async () => {
         const time = new Date().toLocaleTimeString();
+        console.log(`\n\n-- Tool: TIME ${time} --\n\n`)
         return {
           time,
         };
@@ -22,9 +23,25 @@ export function createTools() {
       }),
       execute: async ({location}) => {
         const temperature = Math.round(Math.random() * (90 - 32) + 32);
+        console.log(`\n\n-- Tool: GET WEATHER ${location} ${temperature} --\n\n`)
         return {
           location,
           temperature,
+        };
+      },
+    }),
+    convertFahrenheitToCelsius: tool({
+      description: 'Convert a temperature in fahrenheit to celsius',
+      inputSchema: z.object({
+        temperature: z
+          .number()
+          .describe('The temperature in fahrenheit to convert'),
+      }),
+      execute: async ({ temperature }) => {
+        const celsius = Math.round((temperature - 32) * (5 / 9));
+        console.log(`\n\n-- Tool: TEMP CONVERT ${temperature} -> ${celsius} --\n\n`)
+        return {
+          celsius,
         };
       },
     }),
